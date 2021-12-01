cea6b58637b0900ed305b083558406563d54edc1
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-12-02 02:13:54 +0700

    (kylix): set up bottom sheet fragment to set schedule time, delete navigation arch, change theme color, and add some images from figma.

--- app/src/main/java/com/example/testprojek/ui/schedule/NewScheduleViewModel.kt
+++ app/src/main/java/com/example/testprojek/ui/schedule/NewScheduleViewModel.kt
@@ -2,13 +2,10 @@ package com.example.testprojek.ui.schedule
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.asLiveData
 import com.example.testprojek.data.ScheduleDao
 import com.example.testprojek.data.ScheduleDatabase
-import com.example.testprojek.util.Day
-import kotlinx.coroutines.Dispatchers
 
-class ScheduleViewModel(application: Application): AndroidViewModel(application) {
+class NewScheduleViewModel(application: Application) : AndroidViewModel(application) {
 
     private val dao: ScheduleDao
 
@@ -16,6 +13,4 @@ class ScheduleViewModel(application: Application): AndroidViewModel(application)
         val database = ScheduleDatabase.getInstance(application)
         dao = database.scheduleDao()
     }
-
-    fun getAllSchedule(day: Day) = dao.getAllSchedule(day).asLiveData(Dispatchers.IO)
 }
--- app/src/main/java/com/example/testprojek/ui/schedule/ScheduleActivity.kt
+++ app/src/main/java/com/example/testprojek/ui/schedule/ScheduleActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.testprojek.R
 
-class ScheduleAcitivity : AppCompatActivity() {
+class ScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
